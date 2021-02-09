5eb1ae3dc5ea7809ea9f97cde83381d72d1da727
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2021-02-09 15:23:39 +0100

    Dividing roles

--- docker-entrypoint-sipp.sh
+++ docker-entrypoint-sipp.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-while true; do
-    echo -n "This is a test in loop $i "
-    date
-    sleep 1
-done
+if [[ "${ROLE}" == "UAS" ]]; then
+    /usr/bin/sipp -sf /opt/sipp_uas.xml -r 1 -l 1 -m 1
+else
+# This is UAC
+    while true; do
+        echo -n "This is a loop for keeping container alive"
+        date
+        sleep 10
+    done
+fi
